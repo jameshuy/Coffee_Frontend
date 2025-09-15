cf338c6203154343471ca9c7f5fa5faff328d001
jameshui <jhui9034.dev@gmail.com>
2025-09-16 00:33:17 +0800

    fix: Dashboard Img src

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -11,6 +11,8 @@ import SubscriptionModal from "@/components/SubscriptionModal";
 
 interface UserImage {
   id: string;
+  thumbnailPath: string;
+  originalPath: string;
   imageUrl: string;
   fullImageUrl: string;
   style: string;
@@ -101,7 +103,7 @@ export default function Dashboard() {
               <DashboardImageModal
                 isOpen={!!selectedImage}
                 onClose={closeImageModal}
-                imageUrl={import.meta.env.VITE_API_URL + selectedImage.imageUrl}
+                imageUrl={selectedImage.thumbnailPath}
                 style={selectedImage.style}
                 id={selectedImage.id}
                 isPublic={selectedImage.isPublic}
@@ -123,7 +125,7 @@ export default function Dashboard() {
                   <div className="w-full bg-black" style={{ aspectRatio: '1/1.414' }}>
                     <div className="w-full h-full relative group hover:outline hover:outline-[12px] hover:outline-white transition-all duration-300">
                       <img 
-                        src={import.meta.env.VITE_API_URL + image.imageUrl} 
+                        src={image.thumbnailPath} 
                         alt={`Poster with ${image.style} style`}
                         className="w-full h-full object-fill transition-all duration-300 select-none pointer-events-none"
                         loading="lazy"
